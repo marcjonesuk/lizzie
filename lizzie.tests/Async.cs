6cbd0a52b4f3e2786b54e7e228f1a494fce7999a
marc.jones <marc.jones@spreadex.com>
2019-05-24 12:06:49 +0100

    Adding async test

---
+++ lizzie.tests/Async.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lizzie.tests
+{
+    class AsyncClass
+    {
+        [Bind(Name = "foo")]
+        async Task<object> Foo(Binder<AsyncClass> ctx, Arguments arguments)
+        {
+            var x = 10;
+            await Task.Run(() => x++);
+            x++;
+            return x;
+        }
+    }
+
+    class Async
+    {
+        [Test]
+        public async Task TaskIsAwaited()
+        {
+            AsyncClass simple = new AsyncClass();
+            var lambda = LambdaCompiler.Compile(simple, "foo()", true);
+            var result = await lambda();
+            Assert.AreEqual(12, result);
+        }
+    }
+}
